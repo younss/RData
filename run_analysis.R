c1c7ebda4bef467159b3fb112583c19ca697ff20
Younss <younssiga@gmail.com>
2015-04-20 13:23:10 -0400

    run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,40 @@
-library(plyr);
-#setwd("D:/Data/atelier_genie_logiciel/r_workspace")
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+if (!require("reshape2")) {
+  install.packages("reshape2")
+}
+require("data.table")
+require("reshape2")
 dir.create("./data")
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 path_rf <- file.path("./data" , "UCI HAR Dataset")
-files<-list.files(path_rf, recursive=TRUE)
-#files
-dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
-dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
-dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
-dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
-#str(dataActivityTest)
-dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
-dataActivity<- rbind(dataActivityTrain, dataActivityTest)
-dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
-names(dataSubject)<-c("subject")
-names(dataActivity)<- c("activity")
-dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
-names(dataFeatures)<- dataFeaturesNames$V2
-dataCombine <- cbind(dataSubject, dataActivity)
-Data <- cbind(dataFeatures, dataCombine)
-subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
-selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
-Data<-subset(Data,select=selectedNames)
-activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
-head(Data$activity,30)
-names(Data)<-gsub("^t", "time", names(Data))
-names(Data)<-gsub("^f", "frequency", names(Data))
-names(Data)<-gsub("Acc", "Accelerometer", names(Data))
-names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
-names(Data)<-gsub("Mag", "Magnitude", names(Data))
-names(Data)<-gsub
-names(Data)
-
-Data2<-aggregate(. ~subject + activity, Data, mean)
-Data2<-Data2[order(Data2$subject,Data2$activity),]
-write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+activity_labels <- read.table(file.path(path_rf, "activity_labels.txt"))[,2]
+features <- read.table(file.path(path_rf, "features.txt"))[,2]
+extract_features <- grepl("mean|std", features)
+X_test <- read.table(file.path(path_rf, "test" , "X_test.txt"))
+y_test <- read.table(file.path(path_rf, "test" , "y_test.txt"))
+subject_test <- read.table(file.path(path_rf, "test" , "subject_test.txt"))
+names(X_test) = features
+X_test = X_test[,extract_features]
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+X_train <- read.table(file.path(path_rf, "train", "X_train.txt"))
+y_train <- read.table(file.path(path_rf, "train", "Y_train.txt"))
+subject_train <- read.table(file.path(path_rf, "train", "subject_train.txt"))
+names(X_train) = features
+X_train = X_train[,extract_features]
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+data = rbind(test_data, train_data)
+id_labels = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data = melt(data, id = id_labels, measure.vars = data_labels)
+tidy_data = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+write.table(tidy_data, file = "./tidydata.txt")
